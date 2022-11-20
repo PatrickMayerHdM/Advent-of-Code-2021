d599a63b87e2304d6b4187e7b0b14d882a4d8eec
ptrckmyr <pmayer2003@gmail.com>
2022-11-20 23:00:06 +0100

    Day 1

--- Day 1/part 1.py
+++ Day 1/part 1.py
@@ -1,3 +1,21 @@
 import pathlib
-text = pathlib.Path('input.txt').read_text()
-print(text)
+text = pathlib.Path('Day 1\input_test.txt').read_text().split('\n')
+numbers = [int(line) for line in text]
+
+i = 0
+o = 1
+higher = 0
+
+while i < len(numbers):
+  if numbers[o] > numbers[i]:
+    print(True)
+    i = i + 1
+    o = o + 1
+
+  else:
+    print(False)
+    i = i + 1
+    o = o + 1
+
+
+
