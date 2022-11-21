77f2460bcf582d822e83b843cd8a737b76fe5162
ptrckmyr <pmayer2003@gmail.com>
2022-11-21 22:32:20 +0100

    Day 1 Part 1

--- Day 1 Part 1/part 1.py
+++ Day 1 Part 1/part 1.py
@@ -1,21 +1,19 @@
 import pathlib
-text = pathlib.Path('Day 1\input_test.txt').read_text().split('\n')
+text = pathlib.Path('Day 1 Part 1\input_test.txt').read_text().split('\n')
 numbers = [int(line) for line in text]
 
 i = 0
 o = 1
 higher = 0
 
-while i < len(numbers):
+while o < len(numbers):
   if numbers[o] > numbers[i]:
-    print(True)
+    higher = higher + 1
     i = i + 1
     o = o + 1
 
   else:
-    print(False)
     i = i + 1
     o = o + 1
 
-
-
+print(f"Higher ist jetzt; {higher}")
